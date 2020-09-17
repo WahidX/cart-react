4b60a82fe2fc04c1074efb88a22f32a96db427f6
WahidX <wahidx93@gmail.com>
2020-09-18 04:48:14 +0530

    CartItem component added

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import './App.css';
+import CartItem from './CartItem';
 
 function App() {
   return (
     <div className="App">
         
       <p>
-        Cart App  
+        <h1>Cart</h1> 
       </p>
       
-          
+      <CartItem/>
         
       
     </div>
